65c03892793ed5f6b766f30c079de3f0b714bf3a
saisreedeep <saisreedeep@outlook.com>
2023-01-26 17:53:59 +0530

    First commit. Included webflux test

---
+++ src/test/java/com/sreedeep/speedtest/SpeedtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreedeep.speedtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
